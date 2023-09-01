12bb48fada69236ea0d05ad890e15359e707100b
Stendhalsynd <qmflf556@naver.com>
2023-09-01 13:20:37 +0900

    slack bot
    # 작업내용
    
    - 스터디 생성시 슬랙봇 알림
    
    - 슬랙에서 승인 / 거절시 상태 ( APPROVED / REJECTED ) 변경

--- controller/CStudy.js
+++ controller/CStudy.js
@@ -1,4 +1,6 @@
 const { Study, Theme } = require("../models");
+const boltApp = require("../slack");
+const definePayload = require("../utils/payload");
 
 // GET
 exports.getRegister = (req, res) => {
@@ -13,6 +15,8 @@ exports.getList = async (req, res) => {
 // POST
 exports.postRegister = async (req, res) => {
   try {
+    console.log("req.body : ", req.body);
+
     const { memTotal, category, startDate, endDate, title, intro } = req.body;
     // const result = await Study.create(
     //   {
@@ -38,6 +42,27 @@ exports.postRegister = async (req, res) => {
     for (let i = 0; i < category.length; i++) {
       Theme.create({ category: category[i], StudyId });
     }
+
+    const categoryStr = category.reduce((tot, item) => tot + ", " + item);
+
+    // 슬랙 채널로 보낼 데이터 생성
+    const blocks = definePayload(
+      memTotal,
+      startDate,
+      endDate,
+      title,
+      intro,
+      categoryStr,
+      StudyId
+    ).blocks;
+
+    // slack team2-week4-bot 채널로 스터디 생성 요청 알림
+    await boltApp.client.chat.postMessage({
+      token: process.env.SLACK_BOT_TOKEN,
+      channel: process.env.CHANNEL_ID,
+      blocks,
+      text: "스터디 개설 요청",
+    });
 
     res.json({ result: true });
   } catch (error) {
---
+++ slack.js
@@ -0,0 +1,81 @@
+const { App, LogLevel } = require("@slack/bolt");
+const { Study } = require("./models");
+
+let logLevel = LogLevel.INFO;
+
+const {
+  SLACK_BOT_TOKEN: token,
+  SLACK_APP_TOKEN: appToken,
+  SLACK_SIGNING_SECRET: signingSecret,
+} = process.env;
+
+const boltApp = new App({
+  token,
+  logLevel,
+  signingSecret,
+  socketMode: true,
+  appToken,
+});
+
+boltApp.action("approve", async ({ ack, body }) => {
+  ack();
+
+  console.log();
+
+  await Study.update(
+    {
+      status: "ALLOWED",
+    },
+    {
+      where: {
+        id: body.actions[0].value,
+      },
+    }
+  );
+
+  postMessage("스터디 개설을 승낙했습니다.", body);
+});
+
+boltApp.action("reject", async ({ ack, body }) => {
+  ack();
+
+  await Study.update(
+    {
+      status: "REJECTED",
+    },
+    {
+      where: {
+        id: body.actions[0].value,
+      },
+    }
+  );
+  postMessage("스터디 개설을 거절했습니다.", body);
+});
+
+boltApp.command("/approve", async ({ command, ack, say }) => {
+  try {
+    await ack();
+    say("승인하기 명령어가 인식되었습니다!");
+  } catch (error) {
+    console.log("err");
+    console.error(error);
+  }
+});
+
+const postMessage = async (text, body) => {
+  await boltApp.client.chat.postMessage({
+    token,
+    channel: body.channel.id,
+    thread_ts: body.message.ts,
+    text,
+  });
+};
+
+(async () => {
+  const port = 3000;
+  // Start your app
+  await boltApp.start(process.env.PORT || port);
+  console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
+})();
+
+module.exports = boltApp;
