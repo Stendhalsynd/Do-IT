a4c09b47d7eccab898c050c559cc5a5283d6642f
syxxne <tjdus562@naver.com>
2023-09-11 13:17:12 +0900

    과목 선택 페이지 부연 설명 추가

--- controller/Csocket.js
+++ controller/Csocket.js
@@ -1,10 +1,6 @@
 // 소켓 연결하기
 exports.connection = (io, socket) => {
-  console.log("소켓 id", socket.id);
-
   socket.on("sendMessage", (message) => {
-    console.log(message);
-
     socket.emit("newMessage", message.message, message.nick);
   });
 };
