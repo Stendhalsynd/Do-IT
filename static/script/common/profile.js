6f4a9d6c922e538be1050f47ef26a96ec0c00fd0
Stendhalsynd <qmflf556@naver.com>
2023-10-29 16:39:51 +0900

    fix: optional chaining

--- static/script/common/profile.js
+++ static/script/common/profile.js
@@ -1,35 +1,10 @@
 const profileButton = document.querySelector(".header__button--profile");
 
-profileButton?.addEventListener("click", async () => {
-  // 마이페이지로 이동하기
-  const userToken = localStorage.getItem("userToken");
-  if (!userToken) {
-    const Toast = Swal.mixin({
-      toast: true,
-      position: "center",
-      showConfirmButton: false,
-      timer: 2000,
-      timerProgressBar: true,
-      customClass: {
-        container: "custom-swal-container",
-      },
-    });
-
-    Toast.fire({
-      icon: "warning",
-      title: "로그인이 필요합니다.",
-    });
-  } else {
-    const res = await axios({
-      method: "POST",
-      url: "/user/verify",
-      headers: {
-        Authorization: `Bearer ${userToken}`,
-      },
-    });
-    if (res.data.result) {
-      document.location.href = `/user/mypage/${res.data.nickname}`;
-    } else {
+profileButton &&
+  profileButton.addEventListener("click", async () => {
+    // 마이페이지로 이동하기
+    const userToken = localStorage.getItem("userToken");
+    if (!userToken) {
       const Toast = Swal.mixin({
         toast: true,
         position: "center",
@@ -42,10 +17,36 @@ profileButton?.addEventListener("click", async () => {
       });
 
       Toast.fire({
-        icon: "error",
-        title: `${res.data.message}`,
+        icon: "warning",
+        title: "로그인이 필요합니다.",
       });
-      return;
+    } else {
+      const res = await axios({
+        method: "POST",
+        url: "/user/verify",
+        headers: {
+          Authorization: `Bearer ${userToken}`,
+        },
+      });
+      if (res.data.result) {
+        document.location.href = `/user/mypage/${res.data.nickname}`;
+      } else {
+        const Toast = Swal.mixin({
+          toast: true,
+          position: "center",
+          showConfirmButton: false,
+          timer: 2000,
+          timerProgressBar: true,
+          customClass: {
+            container: "custom-swal-container",
+          },
+        });
+
+        Toast.fire({
+          icon: "error",
+          title: `${res.data.message}`,
+        });
+        return;
+      }
     }
-  }
-});
+  });
